2c1c279b557efa817aac3405fa27ef927d87e57a
leandro47 <leandrosilva47@live.com>
2020-12-29 08:40:22 -0300

    created home screen

--- app/Views/application/welcome.php
+++ app/Views/application/welcome.php
@@ -1,10 +1,42 @@
 <div>
     <div class="row">
         <div class="col-sm-12 text-center">
-            <h4>Bem vindo <?= $userName ?></h4>
+            <h3>Bem vindo <?= $userName ?></h3>
         </div>
+    </div>
+
+    <div class="row justify-content-md-center mb-5">
+        <?php if (isset($message)) : ?>
+            <div class="col-sm-12 col-md-6 col-lg-12 ">
+                <div class="alert alert-<?= $statusMessage ?> alert-dismissible fade show text-center" role="alert">
+                    <?php echo $message; ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        <?php endif; ?>
+        <div class="col-12 mb-3">
+            <hr>
+            <h4>Acesso rápido</h4>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 text-center">
+            <a href="#" class="btn btn-2 btn-block" role="button" aria-pressed="true">Nova OS</a>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 text-center">
+        <a href="#" class="btn btn-2 btn-block" role="button" aria-pressed="true">OS em aberto</a>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 text-center">
+        <a href="#" class="btn btn-2 btn-block" role="button" aria-pressed="true">Aprovação de pedidos</a>
+
+        </div>
+    </div>
+
+    <div class="row mt-5 mb-5">
         <div class="col-sm-12 text-center mt-5">
             <img src="<?= base_url('assets/images/pages/home.svg') ?>" class="img-fluid text-center" style="max-height: 350px;" alt="Responsive image">
         </div>
     </div>
+</div>
+
 </div>
